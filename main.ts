181483657f31fd0660e7ef33bfdf3af5c340c715
Muhammad Ahsan <idahsanali@gmail.com>
2024-03-21 05:50:55 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"; 
+
+let answer = await inquirer.prompt([
+{message:"Enter Your firstNumber", type:"number", name:"firstNumber"},
+{message:"Enter Your secondNumber", type:"number", name:"secondNumber"},
+{message:"Select One of the operator perform the operation", type:"list",name:"operator", choices:["Addition","Subtraction","Multiplication","Division"]}
+])
+
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber+answer.secondNumber);
+}else if (answer.operator === "Subtraction`"){
+    console.log(answer.firstNumber-answer.secondNumber);
+}else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber*answer.secondNumber);
+}else if (answer.operator === "Division"){
+    console.log(answer.firstNumber/answer.secondNumber);
+}else {
+    console.log("PLease Correct the operation your answer is invalid");
+    
+}
